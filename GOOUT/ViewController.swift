ce3e8c66d71383ad9e70fe9712a4de3b87def8fa
Y00ujin <yujingim43@gmail.com>
2021-08-27 15:52:40 +0900

    gitignore 파일 추가

---
+++ GOOUT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GOOUT
+//
+//  Created by 김유진 on 2021/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
