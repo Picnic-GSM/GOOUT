f58935965bbede0674879df9ad8557038d68108c
Y00ujin <yujingim43@gmail.com>
2021-08-28 20:13:27 +0900

    UIColor Extension 추가

---
+++ GOOUT/Extension/Extension.swift
@@ -0,0 +1,19 @@
+//
+//  Extension.swift
+//  GOOUT
+//
+//  Created by 김유진 on 2021/08/28.
+//
+
+import UIKit
+
+// MARK: - UIColor Extension
+extension UIColor{
+    static func rgb(red: CGFloat ,green: CGFloat,blue:CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    static func rgba(red: CGFloat ,green: CGFloat,blue:CGFloat, alpha:CGFloat) -> UIColor{
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
+    }
+}
+
