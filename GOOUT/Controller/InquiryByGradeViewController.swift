db64e97f3b9b9fba41b5ecd3319bfa873e596a9c
Y00ujin <yujingim43@gmail.com>
2021-08-30 11:01:08 +0900

    mainTabBarView 추가, lineView 추가 lineView gradient 추가

--- GOOUT/Controller/InquiryByGradeViewController.swift
+++ GOOUT/Controller/InquiryByGradeViewController.swift
@@ -6,13 +6,57 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 class InquiryByGradeViewController : UIViewController {
+    
+    // MARK: - property
+    lazy var mainTabBarView = MainTabBarView()
+    
 
+    // MARK: - lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .blue
+        layoutSetting()
+
+        mainTabBarViewSetting()
+    }
+    
+    // MARK: - layoutSetting
+    func layoutSetting(){
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(mainTabBarView)
+    }
+    
+    // MARK: - mainTabBarViewSetting
+    func mainTabBarViewSetting(){
+        mainTabBarView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(10.15)
+        }
+        
+        mainTabBarView.lineView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalTo(2)
+            make.centerX.equalToSuperview()
+        }
+        
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 2))
+        let gradient = CAGradientLayer()
+
+        gradient.frame = view.bounds
+        gradient.colors = [UIColor(red: 104/255, green: 134/255, blue: 197/255, alpha: 1).cgColor, UIColor(red: 255/255, green: 173/255, blue: 172/255, alpha: 1).cgColor]
+        gradient.locations = [0.0 , 1.0]
+        gradient.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+
+        mainTabBarView.lineView.layer.insertSublayer(gradient, at: 0)
+        
     }
     
 }
---
+++ GOOUT/View/MainTabBarView.swift
@@ -0,0 +1,35 @@
+//
+//  MainTabBarView.swift
+//  GOOUT
+//
+//  Created by 김유진 on 2021/08/30.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class MainTabBarView: UIView{
+    var lineViewGradient: CAGradientLayer!
+    
+    lazy var lineView = UIView().then{
+        $0.backgroundColor = .white
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layoutSetting()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func layoutSetting(){
+        self.backgroundColor = .white
+        
+        self.addSubview(lineView)
+        
+    }
+}
