9a24456b58589bb8923846fb9c06c9123d5c8e35
baegteun <baegteun@gmail.com>
2021-08-30 18:14:29 +0900

    loginTextFieldMaker 추가 emailTextField, passwordTextField 추가

--- GOOUT/View/SigninView.swift
+++ GOOUT/View/SigninView.swift
@@ -13,7 +13,21 @@ class SigninView: UIView{
     // MARK: - Properties
     private let mainBound = UIScreen.main.bounds
     
+    let emailTextField = UITextField().then{
+        $0.placeholder = "이메일을 입력하세요."
+    }
+    lazy var emailView = loginTextFieldView(textField: emailTextField, text: "Email", fontsize: 14)
     
+    let passwordTextField = UITextField().then {
+        $0.placeholder = "비밀번호를 입력하세요."
+        $0.isSecureTextEntry = true
+    }
+    lazy var passwordView = loginTextFieldView(textField: passwordTextField, text: "Password", fontsize: 14)
+    
+    lazy var stack = UIStackView(arrangedSubviews: [emailView, passwordView]).then{
+        $0.axis = .vertical
+        $0.spacing = self.bounds.height*0.035
+    }
     
     
     // MARK: - Lifecycle
@@ -21,10 +35,26 @@ class SigninView: UIView{
         super.init(frame: frame)
         backgroundColor = .white
         configureShadow()
+        addView()
+        configureUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - addView
+    func addView(){
+        addSubview(stack)
+    }
+    
+    // MARK: - configureUI
+    func configureUI(){
+        stack.snp.makeConstraints {
+            $0.left.equalTo(self.snp.left).offset(self.bounds.width*0.168)
+            $0.right.equalTo(self.snp.right).inset(self.bounds.width*0.168)
+            $0.top.equalTo(self.snp.top).offset(self.bounds.height*0.129)
+        }
     }
     
     // MARK: - configureShadow
@@ -63,5 +93,39 @@ class SigninView: UIView{
         shadows.layer.addSublayer(layer0)
         self.addSubview(shapes)
         shapes.layer.addSublayer(layer1)
+    }
+    
+    // MARK: - loginTextFieldViewMaker
+    func loginTextFieldView(textField: UITextField, text: String, fontsize: Int) -> UIView{
+        let view = UIView()
+        let label = UILabel()
+        label.text = text
+        label.textColor = UIColor(red: 0.408, green: 0.525, blue: 0.733, alpha: 1)
+        label.dynamicFont(fontSize: CGFloat(fontsize), currentFontName: "AppleSDGothicNeo-SemiBold")
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.left.equalTo(view.safeAreaLayoutGuide.snp.left)
+        }
+        
+        view.addSubview(textField)
+        textField.snp.makeConstraints {
+            $0.top.equalTo(label.safeAreaLayoutGuide.snp.bottom).offset(9)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(10)
+            $0.left.equalTo(view.safeAreaLayoutGuide.snp.left)
+            $0.right.equalTo(view.safeAreaLayoutGuide.snp.right)
+        }
+        let Divider = UIView()
+        Divider.backgroundColor = UIColor(red: 0.408, green: 0.525, blue: 0.733, alpha: 1)
+        
+        view.addSubview(Divider)
+        
+        Divider.snp.makeConstraints {
+            $0.left.equalTo(view.safeAreaLayoutGuide.snp.left)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            $0.right.equalTo(view.safeAreaLayoutGuide.snp.right)
+            $0.height.equalTo(1)
+        }
+        return view
     }
 }
