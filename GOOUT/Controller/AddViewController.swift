f13a252e3fe2fd4b1f27483bf1b64cd0e39399f4
Y00ujin <yujingim43@gmail.com>
2021-08-30 16:14:26 +0900

    titleView, titleLabel 생성, layout 설정

--- GOOUT/Controller/AddViewController.swift
+++ GOOUT/Controller/AddViewController.swift
@@ -11,6 +11,15 @@ import SnapKit
  
 class AddViewController: UIViewController{
     // MARK: - property
+    lazy var titleView = UIView().then{
+        $0.backgroundColor = UIColor(red: 104/255, green: 134/255, blue: 197/255, alpha: 1)
+    }
+    
+    lazy var titleLabel = UILabel().then{
+        $0.text = "외출/조퇴 추가하기"
+        $0.textColor = UIColor.white
+        $0.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-Bold")
+    }
     
     // MARK: - lifeCycle
     override func viewDidLoad() {
@@ -21,7 +30,19 @@ class AddViewController: UIViewController{
     
     // MARK: - layoutSetting
     func layoutSetting(){
-        self.view.backgroundColor = .blue
+        self.view.backgroundColor = .white
+        self.view.addSubview(titleView)
+        titleView.addSubview(titleLabel)
+        
+        titleView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalToSuperview().dividedBy(10.38)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+        }
         
     }
 }
