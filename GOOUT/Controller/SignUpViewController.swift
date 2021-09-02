417c939c1b3ed3752b96a547752fd63f579eb0f3
hyein0112 <bhi12134@gmail.com>
2021-09-02 23:47:51 +0900

    passwordExsampleLabel 추가

--- GOOUT/Controller/SignUpViewController.swift
+++ GOOUT/Controller/SignUpViewController.swift
@@ -21,24 +21,43 @@ class SignUpViewController: UIViewController {
     }
     lazy var emailLabel = UILabel().then {
         $0.text = "Email"
-        $0.dynamicFont(fontSize: 14, currentFontName: "Apple SD Gothic Neo-SemiBold")
-        $0.textColor = .rgb(red: 255, green: 172, blue: 183)
-    }
-    lazy var passwordLabel = UILabel().then {
-        $0.text = "Password"
-        $0.dynamicFont(fontSize: 14, currentFontName: "Apple SD Gothic Neo-SemiBold")
-        $0.textColor = .rgb(red: 255, green: 172, blue: 183)
-    }
-    lazy var checkPasswordLabel = UILabel().then {
-        $0.text = "Check Password"
-        $0.dynamicFont(fontSize: 14, currentFontName: "Apple SD Gothic Neo-SemiBold")
+        $0.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-SemiBold")
         $0.textColor = .rgb(red: 255, green: 172, blue: 183)
     }
     lazy var emailTextField = UITextField().then {
-        $0.placeholder = "이메일을 입력하세요"
+        $0.placeholder = "이메일을 입력하세요."
         $0.setPlaceholderColor(.rgb(red: 108, green: 108, blue: 108))
     }
     lazy var emailLine = UIView().then {
+        $0.backgroundColor = .rgb(red: 255, green: 172, blue: 183)
+    }
+    lazy var passwordLabel = UILabel().then {
+        $0.text = "Password"
+        $0.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-SemiBold")
+        $0.textColor = .rgb(red: 255, green: 172, blue: 183)
+    }
+    lazy var passwordTextField = UITextField().then {
+        $0.placeholder = "비밀번호를 입력하세요."
+        $0.setPlaceholderColor(.rgb(red: 108, green: 108, blue: 108))
+    }
+    lazy var passwordLine = UIView().then {
+        $0.backgroundColor = .rgb(red: 255, green: 172, blue: 183)
+    }
+    lazy var passwordExsampleLabel = UILabel().then {
+        $0.text = "비밀번호는 8~16자 사이여야되며, 특수문자를 포함해야합니다."
+        $0.textColor = .rgb(red: 151, green: 151, blue: 151)
+        $0.dynamicFont(fontSize: 8, currentFontName: "AppleSDGothicNeo-SemiBold")
+    }
+    lazy var checkPasswordLabel = UILabel().then {
+        $0.text = "Check Password"
+        $0.dynamicFont(fontSize: 14, currentFontName: "AppleSDGothicNeo-SemiBold")
+        $0.textColor = .rgb(red: 255, green: 172, blue: 183)
+    }
+    lazy var checkPasswordTextField = UITextField().then {
+        $0.placeholder = "비밀번호를 한번 더 입력하세요."
+        $0.setPlaceholderColor(.rgb(red: 108, green: 108, blue: 108))
+    }
+    lazy var checkPasswordLine = UIView().then {
         $0.backgroundColor = .rgb(red: 255, green: 172, blue: 183)
     }
     
@@ -48,28 +67,38 @@ class SignUpViewController: UIViewController {
         
     }
   
+    //MARK: configureUI
     func configureUI() {
         addSubView()
         cornerRadius()
         location()
     }
     
+    //MARK: cornerRadius
     func cornerRadius() {
         backgroundView.layer.cornerRadius = 50
         backgroundView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
         backgroundView.layer.masksToBounds = true
     }
     
+    //MARK: addSubView
     func addSubView() {
         self.view.addSubview(signUpLabel)
         self.view.addSubview(backgroundView)
         self.view.addSubview(emailLabel)
-        self.view.addSubview(passwordLabel)
-        self.view.addSubview(checkPasswordLabel)
         self.view.addSubview(emailTextField)
         self.view.addSubview(emailLine)
+        self.view.addSubview(passwordLabel)
+        self.view.addSubview(passwordTextField)
+        self.view.addSubview(passwordLine)
+        self.view.addSubview(checkPasswordLabel)
+        self.view.addSubview(checkPasswordTextField)
+        self.view.addSubview(checkPasswordLine)
+        self.view.addSubview(passwordExsampleLabel)
     }
     
+
+    //MARK: location
     func location() {
         signUpLabel.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(self.view.frame.height/7.96)
@@ -80,26 +109,51 @@ class SignUpViewController: UIViewController {
             make.height.equalToSuperview().dividedBy(1.33)
             make.top.equalToSuperview().offset(self.view.frame.height/4)
         }
+        
         emailLabel.snp.makeConstraints { make in
             make.top.equalTo(backgroundView).offset(self.view.frame.height/10.15)
             make.left.equalToSuperview().offset(self.view.frame.width/5.95)
         }
-        passwordLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(self.view.frame.height/2.28)
-            make.left.equalTo(emailLabel)
-        }
-        checkPasswordLabel.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(self.view.frame.height/1.85)
-            make.left.equalTo(emailLabel)
-        }
         emailTextField.snp.makeConstraints { make in
-            make.top.equalToSuperview().offset(self.view.frame.height/2.63)
+            make.top.equalTo(emailLabel.snp.bottom).offset(self.view.frame.height/90.22)
             make.left.equalTo(emailLabel)
         }
         emailLine.snp.makeConstraints { make in
             make.width.equalToSuperview().dividedBy(1.5)
             make.height.equalToSuperview().dividedBy(812)
-            make.top.equalToSuperview().offset(self.view.frame.height/2.46)
+            make.top.equalTo(emailLabel.snp.bottom).offset(self.view.frame.height/24.61)
+            make.centerX.equalToSuperview()
+        }
+        
+        passwordLabel.snp.makeConstraints { make in
+            make.top.equalTo(backgroundView).offset(self.view.frame.height/5.31)
+            make.left.equalTo(emailLabel)
+        }
+        passwordTextField.snp.makeConstraints { make in
+            make.top.equalTo(passwordLabel.snp.bottom).offset(self.view.frame.height/90.22)
+            make.left.equalTo(passwordLabel)
+        }
+        passwordLine.snp.makeConstraints { make in
+            make.width.height.equalTo(emailLine)
+            make.top.equalTo(passwordLabel.snp.bottom).offset(self.view.frame.height/24.61)
+            make.centerX.equalToSuperview()
+        }
+        passwordExsampleLabel.snp.makeConstraints { make in
+            make.top.equalTo(passwordLabel.snp.bottom).offset(self.view.frame.height/20.82)
+            make.left.equalTo(passwordLabel)
+        }
+        
+        checkPasswordLabel.snp.makeConstraints { make in
+            make.top.equalTo(backgroundView).offset(self.view.frame.height/3.44)
+            make.left.equalTo(emailLabel)
+        }
+        checkPasswordTextField.snp.makeConstraints { make in
+            make.top.equalTo(checkPasswordLabel.snp.bottom).offset(self.view.frame.height/90.22)
+            make.left.equalTo(passwordLabel)
+        }
+        checkPasswordLine.snp.makeConstraints { make in
+            make.width.height.equalTo(emailLine)
+            make.top.equalTo(checkPasswordLabel.snp.bottom).offset(self.view.frame.height/24.61)
             make.centerX.equalToSuperview()
         }
         
