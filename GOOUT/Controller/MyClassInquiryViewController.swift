977281548b8b56777df57ba07ac2f0429db5bd3b
이동현 <81354005+donghyun3996@users.noreply.github.com>
2021-09-02 19:48:33 +0900

    Revert "Revert 3 feature/forgot pw"

---
+++ GOOUT/Controller/MyClassInquiryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MyClassInquiryViewController.swift
+//  GOOUT
+//
+//  Created by 김유진 on 2021/08/30.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class MyClassInquiryViewController: UIViewController{
+    // MARK: - property
+    lazy var mainTabBarView = MainTabBarView()
+    
+    // MARK: - lifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        layoutSetting()
+    }
+    
+    // MARK: - layoutSetting
+    func layoutSetting(){
+        self.view.backgroundColor = .white
+        
+    }
+}
