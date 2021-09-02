1e3325437e78cd88a7be646e5ea1013441ca3d8d
Y00ujin <yujingim43@gmail.com>
2021-09-02 17:19:49 +0900

    외출 버튼 클릭 시 외출 시간 부분 alpha값 변경 구현

--- GOOUT/Controller/AddViewController.swift
+++ GOOUT/Controller/AddViewController.swift
@@ -51,99 +51,10 @@ class AddViewController: UIViewController{
         $0.textColor = UIColor(red: 105/255, green: 105/255, blue: 105/255, alpha: 1)
     }
     
-    lazy var studentNameLabel = UILabel().then{
-        $0.text = "학생이름"
-        $0.textColor = UIColor(red: 105/255, green: 105/255, blue: 105/255, alpha: 1)
-        $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-SemiBold")
-    }
-    
-    lazy var studentNameTextField = UITextField().then{
-        $0.placeholder = "이름을 입력해주세요."
-        $0.dynamicFont(fontSize: 11, currentFontName: "AppleSDGothicNeo-Light")
-    }
-    
-    lazy var studentNameTextFieldUnderLineView = UIView().then{
-        $0.backgroundColor = UIColor(red: 196/255, green: 196/255, blue: 196/255, alpha: 1)
-    }
-    
-    lazy var classLabel = UILabel().then{
-        $0.text = "반"
-        $0.textColor = UIColor(red: 105/255, green: 105/255, blue: 105/255, alpha: 1)
-        $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-SemiBold")
-    }
-    
-    lazy var numberLabel = UILabel().then{
-        $0.text = "번호"
-        $0.textColor = UIColor(red: 105/255, green: 105/255, blue: 105/255, alpha: 1)
-        $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-SemiBold")
-    }
-    
     lazy var gooutTimeLabel = UILabel().then{
         $0.text = "외출시간"
         $0.textColor = UIColor(red: 105/255, green: 105/255, blue: 105/255, alpha: 1)
         $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-SemiBold")
-    }
-    
-    lazy var classButton = UIButton().then{
-        $0.layer.borderWidth = 0.6
-        $0.layer.borderColor = UIColor(red: 196/255, green: 196/255, blue: 196/255, alpha: 1).cgColor
-        $0.layer.cornerRadius = 7
-    }
-    
-    lazy var selectedClassLabel = UILabel().then{
-        $0.text = "1반"
-        $0.dynamicFont(fontSize: 10, currentFontName: "AppleSDGothicNeo-Light")
-        $0.textColor = UIColor(red: 112/255, green: 112/255, blue: 112/255, alpha: 1)
-    }
-    
-    lazy var classUnderButton = UIButton().then{
-        $0.setImage(UIImage(named: "GOOUT_UnderButtonImage"), for: .normal)
-    }
-    
-    lazy var classDropDown = DropDown().then{
-        $0.dataSource = ["1반", "2반", "3반", "4반"]
-        $0.anchorView = classButton
-        $0.bottomOffset = CGPoint(x: 0, y: self.view.frame.height/20)
-        $0.textFont = UIFont(name: "AppleSDGothicNeo-Light", size: 10)!
-        $0.backgroundColor = UIColor.white
-        $0.cornerRadius = 7
-        $0.cellHeight = self.view.frame.height/26
-        $0.shadowColor = UIColor.lightGray
-        $0.shadowOffset = CGSize(width: 0, height: 3)
-        $0.shadowRadius = 10
-        $0.shadowOpacity = 0.3
-    }
-    
-    lazy var numberButton = UIButton().then{
-        $0.layer.borderWidth = 0.6
-        $0.layer.borderColor = UIColor(red: 196/255, green: 196/255, blue: 196/255, alpha: 1).cgColor
-        $0.layer.cornerRadius = 7
-    }
-    
-    lazy var selectedNumberLabel = UILabel().then{
-        $0.text = "1번"
-        $0.dynamicFont(fontSize: 10, currentFontName: "AppleSDGothicNeo-Light")
-        $0.textColor = UIColor(red: 112/255, green: 112/255, blue: 112/255, alpha: 1)
-    }
-    
-    lazy var numberUnderButton = UIButton().then{
-        $0.setImage(UIImage(named: "GOOUT_UnderButtonImage"), for: .normal)
-    }
-    
-    lazy var numberDropDown = DropDown().then{
-        $0.dataSource = ["1번", "2번", "3번", "4번","5번", "6번", "7번", "8번","9번", "10번", "11번", "12번","13번", "14번", "15번", "16번","17번", "18번", "19번", "20번"]
-        $0.anchorView = numberButton
-        $0.direction = .bottom
-        $0.offsetFromWindowBottom = self.view.frame.height/3.5
-        $0.bottomOffset = CGPoint(x: 0, y: self.view.frame.height/20)
-        $0.textFont = UIFont(name: "AppleSDGothicNeo-Light", size: 10)!
-        $0.backgroundColor = UIColor.white
-        $0.cornerRadius = 7
-        $0.cellHeight = self.view.frame.height/26
-        $0.shadowColor = UIColor.lightGray
-        $0.shadowOffset = CGSize(width: 0, height: 3)
-        $0.shadowRadius = 10
-        $0.shadowOpacity = 0.3
     }
     
     lazy var gooutStartTimeButton = UIButton().then{
@@ -248,6 +159,17 @@ class AddViewController: UIViewController{
             earlyLeaveButton.setImage(UIImage(named: "GOOUT_CheckButtonImage"), for: .normal)
             earlyLeaveButton.isSelected.toggle()
             gooutButton.isSelected.toggle()
+            
+            UIView.animate(withDuration: 0.2) {
+                self.gooutTimeLabel.alpha = 1
+                self.gooutStartTimeButton.alpha = 1
+                self.selectedGooutStartTimeLabel.alpha = 1
+                self.gooutStartTimeUnderButton.alpha = 1
+                self.waveLabel.alpha = 1
+                self.gooutEndTimeButton.alpha = 1
+                self.selectedGooutEndTimeLabel.alpha = 1
+                self.gooutEndTimeUnderButton.alpha = 1
+            }
 
         }
     }
@@ -259,19 +181,21 @@ class AddViewController: UIViewController{
             gooutButton.isSelected.toggle()
             earlyLeaveButton.isSelected.toggle()
 
+            UIView.animate(withDuration: 0.2) {
+                self.gooutTimeLabel.alpha = 0
+                self.gooutStartTimeButton.alpha = 0
+                self.selectedGooutStartTimeLabel.alpha = 0
+                self.gooutStartTimeUnderButton.alpha = 0
+                self.waveLabel.alpha = 0
+                self.gooutEndTimeButton.alpha = 0
+                self.selectedGooutEndTimeLabel.alpha = 0
+                self.gooutEndTimeUnderButton.alpha = 0
+            }
         }
     }
     
     @objc func gooutEndTimeButtonClicked(sender:UIButton){
         gooutEndTimeDropDown.show()
-    }
-    
-    @objc func numberButtonClicked(sender:UIButton){
-        numberDropDown.show()
-    }
-    
-    @objc func classButtonClicked(sender:UIButton){
-        classDropDown.show()
     }
     
     @objc func gooutStartTimeButtonClicked(sender:UIButton){
@@ -287,43 +211,11 @@ class AddViewController: UIViewController{
     }
     
     @objc func keyboardWillShow(_ sender: Notification) {
-        UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut, animations: {
-            self.reasonTextView.snp.remakeConstraints { make in
-                make.left.equalTo(self.reasonLabel).offset(-self.view.frame.width/80)
-                make.centerX.equalToSuperview()
-                make.top.equalTo(self.classButton)
-                make.height.equalToSuperview().dividedBy(13)
-            }
-            
-            self.reasonLabel.snp.remakeConstraints { make in
-                make.left.equalTo(self.gooutTimeLabel)
-                make.top.equalTo(self.numberLabel)
-            }
-            
-            self.numberLabel.alpha = 0
-            
-            self.reasonTextView.superview?.layoutIfNeeded()
-        })
+        
     }
     
     @objc func keyboardWillHide(_ sender: Notification) {
-        UIView.animate(withDuration: 1.0, delay: 0, options: .curveEaseOut, animations: {
-            self.reasonTextView.snp.remakeConstraints { make in
-                make.left.equalTo(self.reasonLabel).offset(-self.view.frame.width/80)
-                make.centerX.equalToSuperview()
-                make.top.equalTo(self.reasonLabel.snp.bottom)
-                make.height.equalToSuperview().dividedBy(13)
-            }
-            
-            self.reasonLabel.snp.remakeConstraints { make in
-                make.left.equalTo(self.gooutTimeLabel)
-                make.top.equalTo(self.gooutStartTimeButton.snp.bottom).offset(self.view.frame.height/18.04)
-            }
-            
-            self.numberLabel.alpha = 1
-            
-            self.reasonTextView.superview?.layoutIfNeeded()
-        })
+
     }
     
     // MARK: - reasonTextViewSetting
@@ -347,20 +239,7 @@ class AddViewController: UIViewController{
         whiteView.addSubview(earlyLeaveButton)
         whiteView.addSubview(gooutLabel)
         whiteView.addSubview(earlyLeaveLabel)
-        self.view.addSubview(studentNameLabel)
-        self.view.addSubview(studentNameTextField)
-        self.view.addSubview(studentNameTextFieldUnderLineView)
-        self.view.addSubview(classLabel)
-        self.view.addSubview(numberLabel)
         self.view.addSubview(gooutTimeLabel)
-        self.view.addSubview(classButton)
-        self.view.addSubview(selectedClassLabel)
-        self.view.addSubview(classUnderButton)
-        self.view.addSubview(classDropDown)
-        self.view.addSubview(numberButton)
-        self.view.addSubview(selectedNumberLabel)
-        self.view.addSubview(numberUnderButton)
-        self.view.addSubview(numberDropDown)
         self.view.addSubview(gooutStartTimeButton)
         self.view.addSubview(selectedGooutStartTimeLabel)
         self.view.addSubview(gooutStartTimeUnderButton)
@@ -381,10 +260,6 @@ class AddViewController: UIViewController{
         
         gooutButton.addTarget(self, action: #selector(gooutButtonClicked(sender:)), for: .touchUpInside)
         earlyLeaveButton.addTarget(self, action: #selector(earlyLeaveButtonClicked(sender:)), for: .touchUpInside)
-        classButton.addTarget(self, action: #selector(classButtonClicked(sender:)), for: .touchUpInside)
-        classUnderButton.addTarget(self, action: #selector(classButtonClicked(sender:)), for: .touchUpInside)
-        numberButton.addTarget(self, action: #selector(numberButtonClicked(sender:)), for: .touchUpInside)
-        numberUnderButton.addTarget(self, action: #selector(numberButtonClicked(sender:)), for: .touchUpInside)
         gooutStartTimeButton.addTarget(self, action: #selector(gooutStartTimeButtonClicked(sender:)), for: .touchUpInside)
         gooutStartTimeUnderButton.addTarget(self, action: #selector(gooutStartTimeButtonClicked(sender:)), for: .touchUpInside)
         gooutEndTimeButton.addTarget(self, action: #selector(gooutEndTimeButtonClicked(sender:)), for: .touchUpInside)
@@ -394,16 +269,6 @@ class AddViewController: UIViewController{
         gooutEndTimeDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
             selectedGooutEndTimeLabel.text = "\(item)"
             self.gooutEndTimeDropDown.clearSelection()
-        }
-        
-        classDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
-            selectedClassLabel.text = "\(item)"
-            self.classDropDown.clearSelection()
-        }
-        
-        numberDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
-            selectedNumberLabel.text = "\(item)"
-            self.numberDropDown.clearSelection()
         }
         
         gooutStartTimeDropDown.selectionAction = { [unowned self] (index: Int, item: String) in
@@ -453,87 +318,23 @@ class AddViewController: UIViewController{
             make.right.equalTo(earlyLeaveLabel.snp.left).offset(-self.view.frame.width/38.33)
         }
         
-        studentNameLabel.snp.makeConstraints { make in
-            make.top.equalTo(whiteView.snp.bottom).offset(self.view.frame.height/14)
-            make.left.equalToSuperview().offset(self.view.frame.width/6.7)
-        }
-        
-        studentNameTextField.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.left.equalTo(studentNameLabel)
-            make.height.equalToSuperview().dividedBy(23)
-            make.top.equalTo(studentNameLabel.snp.bottom)
-        }
-        
-        studentNameTextFieldUnderLineView.snp.makeConstraints { make in
-            make.top.equalTo(studentNameTextField.snp.bottom)
-            make.width.equalTo(studentNameTextField)
-            make.height.equalTo(0.6)
-            make.centerX.equalToSuperview()
-        }
-        
-        classLabel.snp.makeConstraints { make in
-            make.left.equalTo(studentNameTextFieldUnderLineView)
-            make.top.equalTo(studentNameTextFieldUnderLineView.snp.bottom).offset(self.view.frame.height/26.92)
-        }
-        
-        classButton.snp.makeConstraints { make in
-            make.left.equalTo(classLabel)
-            make.width.equalToSuperview().dividedBy(3.52)
-            make.height.equalToSuperview().dividedBy(25.96)
-            make.top.equalTo(classLabel.snp.bottom).offset(self.view.frame.height/103.85)
-        }
-        
-        selectedClassLabel.snp.makeConstraints { make in
-            make.left.equalTo(classButton).offset(self.view.frame.width/26.53)
-            make.centerY.equalTo(classButton)
-        }
-        
-        classUnderButton.snp.makeConstraints { make in
-            make.right.equalTo(classButton).offset(-self.view.frame.width/50)
-            make.centerY.equalTo(classButton)
-            make.height.equalTo(classButton).dividedBy(1.2)
-            make.width.equalTo(classUnderButton.snp.height)
-        }
-        
-        numberButton.snp.makeConstraints { make in
-            make.right.equalTo(studentNameTextFieldUnderLineView)
-            make.height.width.top.equalTo(classButton)
-        }
-        
-        selectedNumberLabel.snp.makeConstraints { make in
-            make.left.equalTo(numberButton).offset(self.view.frame.width/26.53)
-            make.centerY.equalTo(numberButton)
-        }
-        
-        numberUnderButton.snp.makeConstraints { make in
-            make.right.equalTo(numberButton).offset(-self.view.frame.width/50)
-            make.centerY.equalTo(numberButton)
-            make.height.equalTo(numberButton).dividedBy(1.2)
-            make.width.equalTo(numberUnderButton.snp.height)
-        }
-        
-        numberLabel.snp.makeConstraints { make in
-            make.left.equalTo(numberButton)
-            make.bottom.equalTo(numberButton.snp.top).offset(-self.view.frame.height/103.85)
-        }
-        
         gooutTimeLabel.snp.makeConstraints { make in
-            make.top.equalTo(classButton.snp.bottom).offset(self.view.frame.height/16.15)
-            make.left.equalTo(classButton)
+            make.top.equalTo(whiteView.snp.bottom).offset(self.view.frame.height/16.15)
+            make.left.equalTo(whiteView)
         }
         
         gooutStartTimeButton.snp.makeConstraints { make in
             make.left.equalTo(gooutTimeLabel)
             make.top.equalTo(gooutTimeLabel.snp.bottom).offset(self.view.frame.height/103.85)
-            make.height.width.equalTo(classButton)
+            make.height.equalToSuperview().dividedBy(26.37)
+            make.width.equalToSuperview().dividedBy(3.39)
         }
         
         gooutStartTimeUnderButton.snp.makeConstraints { make in
             make.right.equalTo(gooutStartTimeButton).offset(-self.view.frame.width/50)
             make.centerY.equalTo(gooutStartTimeButton)
             make.height.equalTo(gooutStartTimeButton).dividedBy(1.2)
-            make.width.equalTo(numberUnderButton.snp.height)
+            make.width.equalTo(gooutStartTimeUnderButton.snp.height)
         }
         
         selectedGooutStartTimeLabel.snp.makeConstraints { make in
@@ -549,7 +350,7 @@ class AddViewController: UIViewController{
         gooutEndTimeButton.snp.makeConstraints { make in
             make.centerY.equalTo(gooutStartTimeButton)
             make.width.height.equalTo(gooutStartTimeButton)
-            make.left.equalTo(numberButton)
+            make.right.equalToSuperview().offset(-self.view.frame.width/6.7)
         }
         
         selectedGooutEndTimeLabel.snp.makeConstraints { make in
@@ -594,19 +395,6 @@ class AddViewController: UIViewController{
         gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
 
         titleView.layer.insertSublayer(gradient, at: 0)
-        
-        // MARK: - lineView gradient
-        let view2 = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 2))
-        let gradient2 = CAGradientLayer()
-
-        gradient2.frame = view.bounds
-        gradient2.colors = [UIColor(red: 255/255, green: 173/255, blue: 172/255, alpha: 1).cgColor, UIColor(red: 104/255, green: 134/255, blue: 197/255, alpha: 1).cgColor]
-        gradient2.locations = [0.0 , 1.0]
-        gradient2.startPoint = CGPoint(x: 0.0, y: 1.0)
-        gradient2.endPoint = CGPoint(x: 1.0, y: 1.0)
-
-        studentNameTextFieldUnderLineView.layer.insertSublayer(gradient2, at: 0)
-        studentNameTextFieldUnderLineView.clipsToBounds = true
     }
 }
 
