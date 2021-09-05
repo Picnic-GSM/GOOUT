aa5b3756d8f75a577e9b4eb88d3f1d0ba337a4e2
YeonJu <yjnoh1016@naver.com>
2021-09-05 15:01:00 +0900

    UI 개발 테스트용 미리보기 코드 추가

--- GOOUT/Controller/InquiryByGradeViewController.swift
+++ GOOUT/Controller/InquiryByGradeViewController.swift
@@ -126,3 +126,28 @@ class InquiryByGradeViewController : UIViewController {
     
 }
 
+//MARK: - Preview
+#if DEBUG
+import SwiftUI
+struct InquiryByGradeViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        InquiryByGradeViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct InquiryByGradeViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            InquiryByGradeViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
