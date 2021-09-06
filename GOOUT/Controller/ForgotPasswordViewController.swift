ca99e8f34e2d763f1b34255d7b982d85af4c69e9
donghyun3996 <81354005+donghyun3996@users.noreply.github.com>
2021-09-06 19:53:33 +0900

    메인뷰 생성

--- GOOUT/Controller/ForgotPasswordViewController.swift
+++ GOOUT/Controller/ForgotPasswordViewController.swift
@@ -5,4 +5,60 @@
 //  Created by GSM07 on 2021/09/02.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class ForgotPasswordViewController: UIViewController{
+    
+    var mainview = UIView()
+    let bound = UIScreen.main.bounds
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+       configureUI()
+      
+     
+            
+        }
+        
+    
+    
+    func configureUI(){
+        addview()
+        gradient()
+        location()
+    }
+    
+    func addview(){
+        view.addSubview(mainview)
+    }
+    
+    func location(){
+        mainview.snp.makeConstraints{ make in
+            
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.height.equalTo(bound.height*0.74)
+            make.top.equalTo(bound.height*0.25)
+    }
+    }
+    func gradient(){
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor(red: 0.408, green: 0.525, blue: 0.773, alpha: 1.1).cgColor,
+
+              UIColor(red: 1, green: 0.675, blue: 0.718, alpha: 1).cgColor
+        ]
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.0)
+        gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
+        gradientLayer.cornerRadius = 55
+        gradientLayer.maskedCorners = CACornerMask(arrayLiteral: .layerMinXMinYCorner, .layerMaxXMinYCorner)
+        
+        mainview.layer.addSublayer(gradientLayer)
+    }
+    
+    }
+//
