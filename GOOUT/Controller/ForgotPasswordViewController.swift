86bf833303e22ce3482e8531f0986dba68a04ba3
donghyun3996 <81354005+donghyun3996@users.noreply.github.com>
2021-09-06 22:52:52 +0900

    메인뷰에 라벨들 추가

--- GOOUT/Controller/ForgotPasswordViewController.swift
+++ GOOUT/Controller/ForgotPasswordViewController.swift
@@ -13,6 +13,9 @@ class ForgotPasswordViewController: UIViewController{
     
     var mainview = UIView()
     var mainlabel = UILabel()
+    var passwordlabel = UILabel()
+    var sublable = UILabel()
+    var sublable2 = UILabel()
     let bound = UIScreen.main.bounds
     
     override func viewDidLoad() {
@@ -35,6 +38,9 @@ class ForgotPasswordViewController: UIViewController{
     func addview(){
         view.addSubview(mainview)
         view.addSubview(mainlabel)
+        view.addSubview(passwordlabel)
+        view.addSubview(sublable)
+        view.addSubview(sublable2)
     }
     
     func location(){
@@ -47,13 +53,41 @@ class ForgotPasswordViewController: UIViewController{
     }
         
         mainlabel.text = "Find Password"
-        
         mainlabel.dynamicFont(fontSize: 25, currentFontName: "FugazOne-Regular")
         mainlabel.snp.makeConstraints{ mak in
             mak.top.equalTo(bound.height*0.12)
             mak.left.equalTo(bound.width*0.11)
             mak.height.equalTo(bound.height*0.03)
             mak.width.equalTo(bound.width*0.488)
+            
+            
+        passwordlabel.textColor = .white
+        passwordlabel.dynamicFont(fontSize: 22, currentFontName: "AppleSDGothicNeo-Bold")
+        passwordlabel.text = "비밀번호를 잊으셨나요?"
+        passwordlabel.snp.makeConstraints{ make in
+                make.top.equalTo(mainview.snp.top).offset(bound.height*0.11)
+                make.centerX.equalTo(view)
+                
+            
+            sublable.textAlignment = .center
+            sublable.textColor = .white
+            sublable.dynamicFont(fontSize: 16, currentFontName: "AppleSDGothicNeo-SemiBold")
+            sublable.text = "회원가입 시 사용하신 이메일을 입력해주세요."
+            sublable.snp.makeConstraints{make in
+                make.top.equalTo(mainview.snp.top).offset(bound.height*0.21)
+                make.centerX.equalTo(view)
+                
+                
+            }
+            sublable2.textAlignment = .center
+            sublable2.textColor = .white
+            sublable2.dynamicFont(fontSize: 16, currentFontName: "AppleSDGothicNeo-SemiBold")
+            sublable2.text = "이메일로 인증번호를 보내드리겠습니다."
+            sublable2.snp.makeConstraints{make in
+                make.top.equalTo(sublable.snp.bottom).offset(1)
+                make.centerX.equalTo(view)
+            }
+            }
         }
     }
     func gradient(){
