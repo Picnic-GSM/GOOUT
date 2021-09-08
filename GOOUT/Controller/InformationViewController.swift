26600748af2228bb0b1983ea25761ddf45a463f9
hyein0112 <bhi12134@gmail.com>
2021-09-08 23:27:47 +0900

    기본 세팅

--- GOOUT/Controller/InformationViewController.swift
+++ GOOUT/Controller/InformationViewController.swift
@@ -5,4 +5,42 @@
 //  Created by GSM02 on 2021/09/08.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Then
+
+class InformationViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+
+//MARK: - Preview
+
+#if DEBUG
+import SwiftUI
+struct InformationViewControllerRepresentable: UIViewControllerRepresentable {
+    
+func updateUIViewController(_ uiView: UIViewController,context: Context) {
+        // leave this empty
+}
+    @available(iOS 13.0.0, *)
+    func makeUIViewController(context: Context) -> UIViewController{
+        InformationViewController()
+    }
+}
+@available(iOS 13.0, *)
+struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
+    static var previews: some View {
+        Group {
+            InformationViewControllerRepresentable()
+                .ignoresSafeArea()
+                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
+                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
+        }
+        
+    }
+} #endif
