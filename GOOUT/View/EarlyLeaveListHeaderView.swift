d85b0b1cd19dc896f7f522eacb7133f56987ade3
YeonJu <yjnoh1016@naver.com>
2021-09-13 23:22:06 +0900

    EarlyLeaveListHeaderView 생성

---
+++ GOOUT/View/EarlyLeaveListHeaderView.swift
@@ -0,0 +1,48 @@
+//
+//  EarlyLeaveListHeaderView.swift
+//  GOOUT
+//
+//  Created by 노연주 on 2021/09/13.
+//
+
+import UIKit
+import Then
+import SnapKit
+
+class EarlyLeaveListHeaderView: UIView {
+    lazy var nameLabel = UILabel().then {
+        $0.text = "이름"
+        $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-SemiBold")
+        $0.textColor = .rgb(red: 104, green: 134, blue: 197)
+    }
+    
+    lazy var gradeClassNumLabel = UILabel().then {
+        $0.text = "학년/반/번호"
+        $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-SemiBold")
+        $0.textColor = .rgb(red: 104, green: 134, blue: 197)
+    }
+    
+    lazy var reasonLabel = UILabel().then {
+        $0.text = "조퇴사유"
+        $0.dynamicFont(fontSize: 12, currentFontName: "AppleSDGothicNeo-SemiBold")
+        $0.textColor = .rgb(red: 104, green: 134, blue: 197)
+    }
+    
+    func HeaderViewSetting(screenHeight: CGFloat, screenWidth: CGFloat){
+        nameLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().offset(screenWidth/6.05)
+        }
+        
+        gradeClassNumLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(nameLabel.snp.right).offset(screenWidth/9.38)
+        }
+        
+        reasonLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.equalTo(gradeClassNumLabel.snp.right).offset(screenWidth/5)
+        }
+    }
+}
+
