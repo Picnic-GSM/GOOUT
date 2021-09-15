d3e17198b4cadb007f0b60199cba0c8832c5ddbe
Y00ujin <yujingim43@gmail.com>
2021-09-15 09:29:54 +0900

    myPageTitleLabel 생성

--- GOOUT/Controller/MyPageViewController.swift
+++ GOOUT/Controller/MyPageViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
+    
+    //MARK: - Properties
+    lazy var myPageTitleLabel = UILabel().then {
+        $0.text = "My Page"
+        $0.dynamicFont(fontSize: 25, currentFontName: "FugazOne-Regular")
+        $0.textColor = .rgb(red: 255, green: 168, blue: 179)
+    }
 
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        layoutSetting()
+    }
+    
+    //MARK: - layoutSetting
+    func layoutSetting(){
+        
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(myPageTitleLabel)
+        
+        myPageTitleLabel.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(self.view.frame.width/11.5)
+            make.top.equalToSuperview().offset(self.view.frame.height/11.44)
+        }
+        
     }
 
 }
