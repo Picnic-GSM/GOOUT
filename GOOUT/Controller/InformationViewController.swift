a5246c15fbcc3b04e4807a75f97646708dd48686
Y00ujin <yujingim43@gmail.com>
2021-09-15 12:26:28 +0900

    화면전환구현~

--- GOOUT/Controller/InformationViewController.swift
+++ GOOUT/Controller/InformationViewController.swift
@@ -152,6 +152,8 @@ class InformationViewController: UIViewController, UICollectionViewDelegate, UIC
         self.view.addSubview(loginButton)
         self.view.addSubview(numberCollectionView)
 
+        loginButton.addTarget(self, action: #selector(signUpButtonClicked(sender:)), for: .touchUpInside)
+        signUpButton.addTarget(self, action: #selector(signUpButtonClicked(sender:)), for: .touchUpInside)
     }
     
     func cornerRadius() {
@@ -387,31 +389,10 @@ class InformationViewController: UIViewController, UICollectionViewDelegate, UIC
             class4Button.setTitleColor(.rgb(red: 108, green: 108, blue: 108), for: .normal)
         }
     }
-}
-
-//MARK: - Preview
-
-#if DEBUG
-import SwiftUI
-struct InformationViewControllerRepresentable: UIViewControllerRepresentable {
     
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-    @available(iOS 13.0.0, *)
-    func makeUIViewController(context: Context) -> UIViewController{
-        InformationViewController()
+    @objc
+    func signUpButtonClicked(sender:UIButton){
+        let nextVC = SigninViewController()
+        self.navigationController?.pushViewController(nextVC, animated: true)
     }
 }
-@available(iOS 13.0, *)
-struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            InformationViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
