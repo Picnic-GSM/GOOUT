eeebd2eb7e83b57b00b4ff0cfd8b95cdba65dd7c
Y00ujin <yujingim43@gmail.com>
2021-09-15 20:06:26 +0900

    고쳐보자

--- GOOUT/Controller/InformationViewController.swift
+++ GOOUT/Controller/InformationViewController.swift
@@ -396,30 +396,3 @@ class InformationViewController: UIViewController, UICollectionViewDelegate, UIC
         self.navigationController?.pushViewController(nextVC, animated: true)
     }
 }
-
-//MARK: - Preview
-//
-//#if DEBUG
-//import SwiftUI
-//struct InformationViewControllerRepresentable: UIViewControllerRepresentable {
-//    
-//func updateUIViewController(_ uiView: UIViewController,context: Context) {
-//        // leave this empty
-//}
-//    @available(iOS 13.0.0, *)
-//    func makeUIViewController(context: Context) -> UIViewController{
-//        InformationViewController()
-//    }
-//}
-//@available(iOS 13.0, *)
-//struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
-//    static var previews: some View {
-//        Group {
-//            InformationViewControllerRepresentable()
-//                .ignoresSafeArea()
-//                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-//                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-//        }
-//        
-//    }
-//} #endif
