24e011349b92195011133c740f2303e6e34c58eb
Y00ujin <yujingim43@gmail.com>
2021-09-15 09:25:08 +0900

    MyPageViewController 생성

---
+++ GOOUT/Controller/MyPageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyPageViewController.swift
+//  GOOUT
+//
+//  Created by 김유진 on 2021/09/15.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
